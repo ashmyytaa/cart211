69d91a436a2019021c283d8cc44374a93dc9cabe
ashmyytaa <ashmi@live.ca>
2025-11-23 18:13:52 -0500

    finished all the core elements of my 10 pages website

--- data/js/webNine.js
+++ data/js/webNine.js
@@ -3,6 +3,7 @@
 
 
 var canvas;
+let accepts = [];
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
@@ -20,7 +21,34 @@ function setup() {
 
 function draw() {
     background(26, 19, 59);
+    // Fade out and display all the "ACCEPT" texts
+    for (let a of accepts) {
+        fill(217, 1, 102, a.alpha);
+        textSize(a.size);
+        text("ACCEPT", a.x, a.y);
+
+        // Movement & fade
+        a.x += a.vx;
+        a.y += a.vy;
+        a.alpha -= 4;
+    }
+
+    // Remove faded ones
+    accepts = accepts.filter(a => a.alpha > 0);
 
 
+}
 
+function mouseMoved() {
+    // Spawn multiple "ACCEPT" per movement
+    for (let i = 0; i < 6; i++) {
+        accepts.push({
+            x: mouseX + random(-10, 10),
+            y: mouseY + random(-10, 10),
+            vx: random(-1.5, 1.5),
+            vy: random(-1.5, 1.5),
+            size: random(30, 50),
+            alpha: 255
+        });
+    }
 }
--- data/js/webTen.js
+++ data/js/webTen.js
@@ -4,6 +4,8 @@
 
 
 var canvas;
+let particles = [];
+let centerX, centerY;
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
@@ -15,6 +17,9 @@ function setup() {
     canvas.style('z-index', '-1');
     background(175);
 
+    centerX = width / 2;
+    centerY = height / 2;
+    noStroke();
 
 }
 
@@ -23,5 +28,42 @@ function draw() {
     background(26, 19, 59);
 
 
+    if (frameCount % 2 === 0) {
+        let angle = random(TWO_PI);
+        let radius = random(300, 500);
+        particles.push({
+            angle: angle,
+            radius: radius,
+            angularSpeed: random(0.05, 0.1),
+            radialSpeed: random(0.5, 1),
+            size: random(5, 12),
+            alpha: 255
+        });
+    }
+
+    for (let i = particles.length - 1; i >= 0; i--) {
+        let p = particles[i];
+
+        // Spin
+        p.angle += p.angularSpeed;
+        // Move toward center
+        p.radius -= p.radialSpeed;
+
+        // Convert polar to Cartesian
+        let x = centerX + cos(p.angle) * p.radius;
+        let y = centerY + sin(p.angle) * p.radius;
+
+        // Draw particle
+        fill(217, 1, 102, p.alpha);
+        ellipse(x, y, p.size);
+
+        // Fade out
+        p.alpha -= 2;
+
+        // Remove if invisible or reached center
+        if (p.alpha <= 0 || p.radius <= 0) {
+            particles.splice(i, 1);
+        }
+    }
 
 }
