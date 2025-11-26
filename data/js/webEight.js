5ce49070de5d7988baa5252730fbfc1b8872ffb6
ashmyytaa <ashmi@live.ca>
2025-11-26 01:01:29 -0500

    Added commenting to my code

--- data/js/webEight.js
+++ data/js/webEight.js
@@ -4,8 +4,9 @@
 
 
 var canvas;
-let circles = [];
-let centerX, centerY;
+let circles = []; //empty circles array
+let centerX; ///variables to be used lated on for our spinning animation
+let centerY;
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight); // this is too resize the canvas whenever the browser window gets resized/gets minimized
@@ -17,53 +18,52 @@ function setup() {
     canvas.style('z-index', '-1'); //this is to put the javascript canvas in the background, behind all the html and css
     background(175);
 
-    centerX = width / 2;
-    centerY = height / 2;
-    noStroke();
+    centerX = width / 2; //to delimit the center on thehorizontal way
+    centerY = height / 2; //to delimit the center on the vertical way
+
 
 }
 
 
 function draw() {
-    background(26, 19, 59);
+    background(26, 19, 59); //navy blue bakground for our canvas
 
 
     if (frameCount % 2 === 0) {
-        let angle = random(TWO_PI);
-        let radius = random(300, 500);
-        circles.push({
+        let angle = random(TWO_PI); //random angle within pi/2
+        let radius = random(200, 400); //random radius
+
+        circles.push({ //adding the circles to the array
             angle: angle,
             radius: radius,
-            angularSpeed: random(0.05, 0.1),
-            radialSpeed: random(0.5, 1),
+            speedSpin: random(0.05, 0.1),
+            speedCenter: random(0.5, 1),
             size: random(5, 12),
             alpha: 255
         });
     }
 
     for (let i = circles.length - 1; i >= 0; i--) {
-        let p = circles[i];
 
-        // Spin
-        p.angle += p.angularSpeed;
-        // Move toward center
-        p.radius -= p.radialSpeed;
+        let circle = circles[i]; //referring to the array
 
-        // Convert polar to Cartesian
-        let x = centerX + cos(p.angle) * p.radius;
-        let y = centerY + sin(p.angle) * p.radius;
 
-        // Draw particle
-        fill(255, 16, 240, p.alpha);
-        ellipse(x, y, p.size);
+        circle.angle += circle.speedSpin; //adds spead to our circle
+        circle.radius -= circle.speedCenter; //adds spead towards the circle
 
-        // Fade out
-        p.alpha -= 2;
 
-        // Remove if invisible or reached center
-        if (p.alpha <= 0 || p.radius <= 0) {
-            circles.splice(i, 1);
-        }
+        let x = centerX + cos(circle.angle) * circle.radius; //this is what makes ir spin in a rotational movement
+        let y = centerY + sin(circle.angle) * circle.radius;
+
+        push(); //draws our circles in a spinning motion
+        noStroke();
+        fill(255, 16, 240, circle.alpha);
+        ellipse(x, y, circle.size);
+        pop();
+
+        circle.alpha -= 4; //fades out the color by 4 
+
+
     }
 
 }
